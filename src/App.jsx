500ad4cd6138ec5a82f6c7cdde286bd81425d0a4
saniagoValadez <saniago1340@gmail.com>
2024-12-27 01:39:54 -0600

    Second commit, trying to use router dom

--- src/App.jsx
+++ src/App.jsx
@@ -35,28 +35,42 @@ function App() {
 export default App*/
 
 import React from 'react';
-import Header from './components/Header';
-import About from './components/About';
-import Skills from './components/Skills'; 
-import Projects from './components/Projects';
-import Contact from './components/Contact';
+// import Header from './components/Header';
+// import About from './components/About';
+// import Skills from './components/Skills'; 
+// import Projects from './components/Projects';
+// import Contact from './components/Contact';
 import './App.css';
 import Babout from './components/Babout';
-import Bheader from './components/Bheader';
+import Bheader from './components/Pages/Bheader';
 import Bskills from './components/Bskills';
 import Bfooter from './components/Bfooter';
+import { Route, Routes } from "react-router-dom"
+// import { BrowserRouter as Router, Router } from 'react-router-dom';
 // import './Index.css'
 
 // import Container from './components/Container';
 
+const About = () => <h1>About</h1>
+const Certifications = () => <h1>Certifications</h1>
+const Projects = () => <h1>Container</h1>
+
 function App() {
   return (
     <div > 
+      {/* <Router>
+        <Router></Router>
+      </Router> */}
       {/* className='bg-slate-800 min-h screen' */}
       {/* <h1 className="text-3xl font-bold underline">
       Hello world!
     </h1> */}
       <Bheader />
+      <Routes>
+              <Route path="./Pages/Bheader" element={<About/>} />
+              <Route path="./Pages/Container" element={<Projects/>} />
+              <Route path="./Pages/Certifications" element={<Certifications/>} />
+      </Routes>
       {/* <Header /> */}
       <Babout />
       {/* <About /> */}
@@ -68,6 +82,8 @@ function App() {
     </div>
   );
 }
+
+
 
 export default App;
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import ReactDOM from 'react-dom'
+import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Certifications from './components/Pages/Certifications'; 
+import Container from './components/Pages/Container';
 // import Button from './components/button.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  <BrowserRouter>
     {/* <Button></Button> */}
     <App />
-  </StrictMode>,
+  </BrowserRouter>,
 )
