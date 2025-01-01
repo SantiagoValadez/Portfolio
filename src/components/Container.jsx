dcaafe1871888c2c1299ca1876964c90125bfd9b
saniagoValadez <saniago1340@gmail.com>
2024-12-31 20:03:34 -0600

    adding information to project's web page

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -1,4 +1,9 @@
 import Img06 from '../assets/img/Img06.png'
+import Img07 from '../assets/img/Img07.png'
+import Img08 from '../assets/img/Img08.png'
+import Img09 from '../assets/img/Img09.png'
+
+
 
 const Container = () => {
   return (
@@ -7,41 +12,44 @@ const Container = () => {
   <div class="container px-5 py-24 mx-auto">
     <div class="flex flex-wrap w-full mb-20">
       <div class="lg:w-1/2 w-full mb-6 lg:mb-0">
-        <h1 class="sm:text-3xl text-2xl font-medium title-font mb-2 text-gray-900">Pitchfork Kickstarter Taxidermy</h1>
+        <h1 class="sm:text-3xl text-2xl font-medium title-font mb-2 text-gray-900">PROJECTS</h1>
         <div class="h-1 w-20 bg-indigo-500 rounded"></div>
       </div>
-      <p class="lg:w-1/2 w-full leading-relaxed text-gray-500">Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical gentrify, subway tile poke farm-to-table. Franzen you probably haven't heard of them man bun deep jianbing selfies heirloom prism food truck ugh squid celiac humblebrag.</p>
+      <p class="lg:w-1/2 w-full leading-relaxed text-gray-500">I am a dedicated and enthusiastic self-taught web developer with expertise in HTML, CSS, JavaScript, React, and Java. Through hands-on projects, I have gained practical experience applying these technologies to create efficient, user-friendly web solutions.
+Although I don't have professional work experience yet, I am eager to learn, grow, and bring value to the company that places its trust in me. My goal is to contribute innovative ideas and continuously improve my skills to succeed in the tech industry.
+The projects I have worked on incorporate the following technologies: HTML, CSS, JavaScript, React, and Java
+      </p>  
     </div>
     <div class="flex flex-wrap -m-4">
       <div class="xl:w-1/4 md:w-1/2 p-4">
         <div class="bg-gray-100 p-6 rounded-lg">
           <img class="h-40 rounded w-full object-cover object-center mb-6" src={Img06} alt="content"/>
           <h3 class="tracking-widest text-indigo-500 text-xs font-medium title-font">SUBTITLE</h3>
-          <h2 class="text-lg text-gray-900 font-medium title-font mb-4">Chichen Itza</h2>
+          <h2 class="text-lg text-gray-900 font-medium title-font mb-4">Website for a construction company</h2>
           <p class="leading-relaxed text-base">Fingerstache flexitarian street art 8-bit waistcoat. Distillery hexagon disrupt edison bulbche.</p>
         </div>
       </div>
       <div class="xl:w-1/4 md:w-1/2 p-4">
         <div class="bg-gray-100 p-6 rounded-lg">
-          <img class="h-40 rounded w-full object-cover object-center mb-6" src="https://dummyimage.com/721x401" alt="content"/>
+          <img class="h-40 rounded w-full object-cover object-center mb-6" src={Img07} alt="content"/>
           <h3 class="tracking-widest text-indigo-500 text-xs font-medium title-font">SUBTITLE</h3>
-          <h2 class="text-lg text-gray-900 font-medium title-font mb-4">Colosseum Roma</h2>
+          <h2 class="text-lg text-gray-900 font-medium title-font mb-4 pb-7">To do list in Java</h2>
           <p class="leading-relaxed text-base">Fingerstache flexitarian street art 8-bit waistcoat. Distillery hexagon disrupt edison bulbche.</p>
         </div>
       </div>
       <div class="xl:w-1/4 md:w-1/2 p-4">
         <div class="bg-gray-100 p-6 rounded-lg">
-          <img class="h-40 rounded w-full object-cover object-center mb-6" src="https://dummyimage.com/722x402" alt="content"/>
+          <img class="h-40 rounded w-full object-cover object-center mb-6" src={Img08} alt="content"/>
           <h3 class="tracking-widest text-indigo-500 text-xs font-medium title-font">SUBTITLE</h3>
-          <h2 class="text-lg text-gray-900 font-medium title-font mb-4">Great Pyramid of Giza</h2>
+          <h2 class="text-lg text-gray-900 font-medium title-font mb-4 pb-7">Weather App</h2>
           <p class="leading-relaxed text-base">Fingerstache flexitarian street art 8-bit waistcoat. Distillery hexagon disrupt edison bulbche.</p>
         </div>
       </div>
       <div class="xl:w-1/4 md:w-1/2 p-4">
         <div class="bg-gray-100 p-6 rounded-lg">
-          <img class="h-40 rounded w-full object-cover object-center mb-6" src="https://dummyimage.com/723x403" alt="content"/>
+          <img class="h-40 rounded w-full object-cover object-center mb-6" src={Img09} alt="content"/>
           <h3 class="tracking-widest text-indigo-500 text-xs font-medium title-font">SUBTITLE</h3>
-          <h2 class="text-lg text-gray-900 font-medium title-font mb-4">San Francisco</h2>
+          <h2 class="text-lg text-gray-900 font-medium title-font mb-4 pb-7">Blog</h2>
           <p class="leading-relaxed text-base">Fingerstache flexitarian street art 8-bit waistcoat. Distillery hexagon disrupt edison bulbche.</p>
         </div>
       </div>
