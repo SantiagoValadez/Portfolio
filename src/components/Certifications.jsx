3b754ed0bbddd7a62720e3be6d9d059a5472a943
saniagoValadez <saniago1340@gmail.com>
2025-01-02 01:03:51 -0600

    Adding links

--- src/components/Certifications.jsx
+++ src/components/Certifications.jsx
@@ -18,6 +18,7 @@ const Certifications = () => {
             <h1 class="title-font text-lg font-medium text-gray-900 mb-3">Full Stack Java Developer.</h1>
             <p class="leading-relaxed mb-3">HTML, CSS, JavaScript, React, java and SpringBoot. 150 hours. </p>
             <p class="leading-relaxed mb-3">11/28/23 - 06/27/24</p>
+            <a class="text-indigo-500 inline-flex items-center hover:text-gray-900 cursor-pointer transform transition-all duration-2000" href='https://www.acreditta.com/credential/0753262b-0057-4943-9ec1-e17f9b59aa90'>Learn More</a>
             <div class="flex items-center flex-wrap ">
               <span class="text-gray-400 mr-3 inline-flex items-center lg:ml-auto md:ml-0 ml-auto leading-none text-sm pr-3 py-1 border-r-2 border-gray-200">
                 <svg class="w-4 h-4 mr-1" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round" viewBox="0 0 24 24">
@@ -42,6 +43,7 @@ const Certifications = () => {
             <h1 class="title-font text-lg font-medium text-gray-900 mb-3">Development of programming logic</h1>
             <p class="leading-relaxed mb-3">Using algorithms and program flow to develop programming logic.</p>
             <p class="leading-relaxed mb-3">2023</p>
+            <a class="text-indigo-500 inline-flex items-center hover:text-gray-900 cursor-pointer transform transition-all duration-2000">Learn More</a>
             <div class="flex items-center flex-wrap">
               <span class="text-gray-400 mr-3 inline-flex items-center lg:ml-auto md:ml-0 ml-auto leading-none text-sm pr-3 py-1 border-r-2 border-gray-200">
                 <svg class="w-4 h-4 mr-1" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round" viewBox="0 0 24 24">
@@ -66,6 +68,7 @@ const Certifications = () => {
             <h1 class="title-font text-lg font-medium text-gray-900 mb-3">Programming in java</h1>
             <p class="leading-relaxed mb-3">Basic and advanced JAVA fundamentals to develop.</p>
             <p class="leading-relaxed mb-3">2024</p>
+            <a class="text-indigo-500 inline-flex items-center hover:text-gray-900 cursor-pointer transform transition-all duration-2000">Learn More</a>
             <div class="flex items-center flex-wrap ">
               <span class="text-gray-400 mr-3 inline-flex items-center lg:ml-auto md:ml-0 ml-auto leading-none text-sm pr-3 py-1 border-r-2 border-gray-200">
                 <svg class="w-4 h-4 mr-1" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round" viewBox="0 0 24 24">
